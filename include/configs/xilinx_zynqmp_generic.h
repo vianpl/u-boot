4f3154104823f2897a55ef2536228818cef2f503
Matthias Brugger <mbrugger@suse.com>
2019-03-05 18:09:04 +0100

    zynqmp: generic: fix compilation
    Fix compilation by enabling the corresponding
    configuration options.
    
    Signed-off-by: Matthias Brugger <mbrugger@suse.com>

--- include/configs/xilinx_zynqmp_generic.h
+++ include/configs/xilinx_zynqmp_generic.h
@@ -11,11 +11,6 @@
 #ifndef __CONFIG_ZYNQMP_GENERIC_H
 #define __CONFIG_ZYNQMP_GENERIC_H
 
-/* This file should disappear as soon as xhci is converted to DT enumerated */
-
-#define CONFIG_ZYNQMP_XHCI_LIST {ZYNQMP_USB0_XHCI_BASEADDR, \
-				 ZYNQMP_USB1_XHCI_BASEADDR}
-
 #include <configs/xilinx_zynqmp.h>
 
 #endif /* __CONFIG_ZYNQMP_GENERIC_H */
